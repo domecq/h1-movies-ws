854fe8f20ac34c9c187e2a59daa05f852c5f87d7
Nicolás Peralta <nicolas@h1lab.com>
2012-03-21 10:13:19 -0300

    update showtimes

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ H1movies::Application.routes.draw do
   match 'cines' => 'cines#all'
   match 'cines/insertall' => 'cines#insertAll'
   match 'cines/findnear' => 'cines#findNear'
+  match 'cines/updateshowtimes' => 'cines#updateShowtimes'  
   match 'cines/:cine_id' => 'cines#get'  
   match 'cines/:cine_id/:movie_id' => 'cines#get'  
   match 'cines/whereami/:latitud/:longitud' => 'cines#whereAmI', :constraints => { :latitud => /[\-0-9\.]+/, :longitud => /[\-0-9\.]+/ }    
